32ac866fb5d8df80e05a825b6fc97537b078369d
Josh D Littlefair <j.d.littlefair7@gmail.com>
2024-05-28 10:24:19 +0000

    second attempt

--- bin/openmmMD.py
+++ bin/openmmMD.py
@@ -98,7 +98,7 @@ def setup_system(modeller, forcefield, solvmol: str, no_restraints: bool):
     y = Vol**(1./3.)
     z = Vol**(1./3.)
     #modeller.topology.setUnitCellDimensions((x, y, z))
-    modeller.addSolvent(forcefield, numAdded=Natoms)
+    modeller.addSolvent(forcefield, numAdded = Natoms)
     system = forcefield.createSystem(modeller.topology, nonbondedMethod=PME, nonbondedCutoff=1.0*nanometer, constraints=HBonds)
     if not no_restraints:
         logging.info("Using restraints on backbone")
