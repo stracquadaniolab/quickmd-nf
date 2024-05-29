b18e881adf62f0d47f82106303f34915e772945e
Josh D Littlefair <j.d.littlefair7@gmail.com>
2024-05-29 10:58:17 +0000

    box stabilisation

--- bin/openmmMD.py
+++ bin/openmmMD.py
@@ -112,7 +112,7 @@ def setup_system(modeller, forcefield, solvmol: str, no_restraints: bool):
     return system
 
 def setup_simulation(modeller, system):
-    integrator = mm.LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.001*picoseconds)
+    integrator = mm.LangevinMiddleIntegrator(150*kelvin, 1/picosecond, 0.001*picoseconds)
     simulation = app.Simulation(modeller.topology, system, integrator)
     simulation.context.setPositions(modeller.positions)
     return simulation
@@ -135,7 +135,7 @@ def energy_minimization(modeller):
 def md_nvt(simulation, csvname: str, totalsteps: int, reprate: int, pdbname):
     init_state = simulation.context.getState(getEnergy=True, getPositions=True)
     init_pe = init_state.getPotentialEnergy().value_in_unit(kilocalories_per_mole)
-    simulation.context.setVelocitiesToTemperature(300*kelvin)
+    simulation.context.setVelocitiesToTemperature(150*kelvin)
     simulation.reporters.append(app.PDBReporter(pdbname, reprate))
     simulation.reporters.append(app.StateDataReporter(stdout, reprate, step=True, potentialEnergy=True, temperature=True, volume=True))
     prepdf = {'Step':[], 'Potential Energy (kJ/mole)':[], 'Temperature (K)':[], 'Box Volume (nm^3)':[]}
